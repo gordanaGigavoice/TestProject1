0731884be407436ceca1876ff037958b2f22286a
gordanaGigavoice <gordana.atanasova@gigavoice.com>
2018-07-25 14:35:59 +0200

    here we go again

--- src/demoJenkins/DemoJeninsJob.java
+++ src/demoJenkins/DemoJeninsJob.java
@@ -12,11 +12,12 @@ public class DemoJeninsJob {
 	public void testJenkins() {
 		
 		System.out.println("Welcome to Jenkins, move along!");
-		System.setProperty("webdriver.chrome.driver", "/home/gigavoice/chromedriver");
+/*		System.setProperty("webdriver.chrome.driver", "/home/gigavoice/chromedriver");
 		WebDriver driver = new ChromeDriver();
 		driver.get("http://www.google.com");
 		System.out.println(driver.getTitle());
 		driver.quit();
+	*/
 	}
 	
 	
